cf09c8bb19d51333305e8d90a6ed5d51bf3725e7
AppliedComputingA <andrewsheddan@hotmail.com>
2026-07-23 10:20:32 +1000

    Added Buttons to sidebar, and setup reading a csv fille to add homebrew values to buttons to the sidebar

--- SAT.py
+++ SAT.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from PIL import Image
+import csv
 
 colour1 = "#2E3A35"
 colour2 = "#1A2420"
@@ -7,6 +8,7 @@ colour3 = "#2B463B"
 colour4 = "#000000"
 colour5 = "#873F30"
 colour6 = "#3B6E5E"
+colourButtonHover = "#30594c"
 colour7 = "#0E7D54"
 textColour1 = "#FFFFFF"
 panelColour1 = "#242424"
@@ -48,20 +50,193 @@ class App(ctk.CTk):
  
         self.title("D&D Character Generator")
         self.geometry("1440x960")
-        bg = backgroundImage(self, "forestBackground.png")
-        bg.place(x=0, y=0, relwidth=1, relheight=1)
+        self.appearanceMode = ctk.get_appearance_mode()
+        if self.appearanceMode == "Dark":
+            bg = backgroundImage(self, "forestBackground.png")
+            bg.place(x=0, y=0, relwidth=1, relheight=1)
+        elif self.appearanceMode == "Light":
+            bg = backgroundImage(self, "BFG.jpeg")
+            bg.place(x=0, y=0, relwidth=1, relheight=1)
+
+        #bg.place(x=0, y=0, relwidth=1, relheight=1)
 
         self.buildSidebar()
 
+# ------------------------------------------------------------------
+# Sidebar
+# ------------------------------------------------------------------
+
     def buildSidebar(self):
         self.sidebar = ctk.CTkScrollableFrame(
-            self, width=260, fg_color=colour6, corner_radius=0
+            self, width=260, fg_color=panelColour1, corner_radius=0
         )
         self.sidebar.pack(side="left", fill="y")
 
+    # ------------------------------------------------------------------
+    # Filter Buttons
+    # ------------------------------------------------------------------
 
+        self.filterLabel = ctk.CTkLabel(
+            self.sidebar, 
+            text="Filters", 
+            anchor="w",
+            fg_color="transparent", 
+            font=("Inter", 20, "bold")
+            )
 
+        btnRace = ctk.CTkButton(
+            self.sidebar, text="Race", 
+            font=("Inter", 16, "bold"), 
+            fg_color=colour6, 
+            hover_color=colourButtonHover,
+            corner_radius=8, 
+            height=40, 
+            width=240,
+            anchor="w",
+            border_spacing=10  
+            #, command=button_event
+            )
+        
+        btnClass = ctk.CTkButton(
+            self.sidebar, text="Class", 
+            font=("Inter", 16, "bold"), 
+            fg_color=colour6, 
+            hover_color=colourButtonHover,
+            corner_radius=8, 
+            height=40, 
+            width=240,
+            anchor="w",
+            border_spacing=10,
+
+            #, command=button_event
+            )
+        
+        btnBackground = ctk.CTkButton(
+            self.sidebar, text="Background", 
+            font=("Inter", 16, "bold"), 
+            fg_color=colour6, 
+            hover_color=colourButtonHover,
+            corner_radius=8, 
+            height=40, 
+            width=240,
+            anchor="w",
+            border_spacing=10  
+            #, command=button_event
+            )
+        
+        btnPersonality = ctk.CTkButton(
+            self.sidebar, text="Personality", 
+            font=("Inter", 16, "bold"), 
+            fg_color=colour6, 
+            hover_color=colourButtonHover,
+            corner_radius=8, 
+            height=40, 
+            width=240,
+            anchor="w",
+            border_spacing=10  
+            #, command=button_event
+            )
+
+        btnAppearance = ctk.CTkButton(
+            self.sidebar, text="Appearance", 
+            font=("Inter", 16, "bold"), 
+            fg_color=colour6, 
+            hover_color=colourButtonHover,
+            corner_radius=8, 
+            height=40, 
+            width=240,
+            anchor="w",
+            border_spacing=10  
+            #, command=button_event
+            )
+        
+        btnAlignment = ctk.CTkButton(
+            self.sidebar, text="Alignment", 
+            font=("Inter", 16, "bold"), 
+            fg_color=colour6, 
+            hover_color=colourButtonHover,
+            corner_radius=8, 
+            height=40, 
+            width=240,
+            anchor="w",
+            border_spacing=10  
+            #, command=button_event
+            )
+        
+        btnSkills = ctk.CTkButton(
+            self.sidebar, text="Skills", 
+            font=("Inter", 16, "bold"), 
+            fg_color=colour6, 
+            hover_color=colourButtonHover,
+            corner_radius=8, 
+            height=40, 
+            width=240,
+            anchor="w",
+            border_spacing=10  
+            #, command=button_event
+            )
+        
+    # ------------------------------------------------------------------
+    # Homebrew Buttons
+    # ------------------------------------------------------------------
+
+        self.lblHomebrew = ctk.CTkLabel(
+            self.sidebar, 
+            text="Homebrew", 
+            anchor="w",
+            fg_color="transparent", 
+            font=("Inter", 20, "bold")
+            )
+        
+        intFilterYPad = 2
+        intFilterXPad = 10
+        self.filterLabel.pack()
+        btnRace.pack(pady=intFilterYPad, padx=intFilterXPad)
+        btnClass.pack(pady=intFilterYPad, padx=intFilterXPad)
+        btnBackground.pack(pady=intFilterYPad, padx=intFilterXPad)
+        btnPersonality.pack(pady=intFilterYPad, padx=intFilterXPad)
+        btnAppearance.pack(pady=intFilterYPad, padx=intFilterXPad)
+        btnAlignment.pack(pady=intFilterYPad, padx=intFilterXPad)
+        btnSkills.pack(pady=intFilterYPad, padx=intFilterXPad)
+        self.lblHomebrew.pack(pady=20, padx=intFilterXPad)
+        self.HomebrewSidebar()
+
+    def changeAppearanceMode(self, new_appearance_mode: str):
+        ctk.set_appearance_mode(new_appearance_mode)
+
+    def loadHomebrewFromCSV(self, filePath="HomebrewValues.csv"):
+        lstHomebrewEntries = []
+        try:
+            with open(filePath, newline='', encoding='utf-8') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    lstHomebrewEntries.append(row)
+        except FileNotFoundError:
+            pass
+        print(lstHomebrewEntries)
+        return lstHomebrewEntries
+
+    def HomebrewSidebar(self):
+        self.homebrewEntries = self.loadHomebrewFromCSV()
+        #print(self.homebrewEntries)
+        
+        for entry in self.homebrewEntries:
+            homebrewBtn = ctk.CTkButton(
+                self.sidebar, 
+                text=f"{entry['Name']} ({entry['Type']})",
+            font=("Inter", 16, "bold"), 
+            fg_color=colour6, 
+            hover_color=colourButtonHover,
+            corner_radius=8, 
+            height=40, 
+            width=240,
+            anchor="w",
+            border_spacing=10  
+            #, command=button_event
+            )
+            homebrewBtn.pack(pady=2, padx=10)
 
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+

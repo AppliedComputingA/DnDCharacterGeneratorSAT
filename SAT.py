9bb10c38f5f3d6fdeb7f54a2b483164453832799
Andrew <andrewsheddan@hotmail.com>
2026-08-20 02:16:48 +1000

    Added All Generation Frames, they do not contrain information yet, removed image background due to CTk Restrictions

--- SAT.py
+++ SAT.py
@@ -312,56 +312,75 @@ class App(ctk.CTk):
     def nameFrame(self):
         self.topBar = ctk.CTkFrame(
             self, 
-            height=100, 
+            height=92, 
             fg_color=panelColour1, 
             corner_radius=0
         )
         self.topBar.pack(side="top", fill="x")
         self.topBar.pack_propagate(False)
-
+#-------------------------------------------------------------------------------------------------------------------
         strName = generator.CharacterGenerator().NameGeneration()
         dctClass = generator.CharacterGenerator().generateTrait("Traits/ClassTraits.csv", [])
         dctRace = generator.CharacterGenerator().generateTrait("Traits/RaceTraits.csv", [])
     
+
+
         strClassName = dctClass["Class"]
         strRaceName = dctRace["Race"]
+#-------------------------------------------------------------------------------------------------------------------
+        rowFrame = ctk.CTkFrame(self.topBar, fg_color="transparent")
+        rowFrame.pack(side="left", padx=20, pady=5, anchor="w")
+
+        nameRow = ctk.CTkFrame(rowFrame, fg_color="transparent")
+        nameRow.pack(side="top", anchor="w")
         
 
         lblName = ctk.CTkLabel(
-            self.topBar,    
+            nameRow,    
             text=strName,
-            font=("Inter", 30, "bold")
+            font=("Inter", 50, "bold")
         )
-        lblName.pack(pady=10, side="left", padx=20)
+        lblName.pack(side="left")
+
+        traitRow = ctk.CTkFrame(rowFrame, fg_color="transparent")
+        traitRow.pack(side="left", anchor="w")
 
         lblRace = ctk.CTkLabel(
-            self.topBar,
+            traitRow,
             text=strRaceName,
-            font=("Inter", 10, "bold")
+            font=("Inter", 20, "bold")
         )
-        lblRace.pack(pady=10, side="left", padx=20)
+        lblRace.pack(pady=0, side="left", padx=5)
 
         lblClass = ctk.CTkLabel(
-            self.topBar,
+            traitRow,
             text=strClassName,
-            font=("Inter", 10, "bold")
+            font=("Inter", 20, "bold")
         )
-        lblClass.pack(pady=10, side="left", padx=20)
+        lblClass.pack(pady=0, side="left", padx=5)
 
     def generationFrames(self):
 
+        self.frmGenerationFrame = ctk.CTkFrame(self, fg_color="transparent")
+        self.frmGenerationFrame.pack(side="top", padx=0, pady=0, anchor="w")
+
+        row1 = ctk.CTkFrame(self.frmGenerationFrame, fg_color="transparent")
+        row1.pack(side="top", fill="x", anchor="w")
+
         dctStats = generator.CharacterGenerator().statGeneration()
 
-        for key, value in dctStats.items():
+        for i, (key, value) in enumerate(dctStats.items()):
+            row1.columnconfigure(i, weight=1, uniform="statcol")
+
             self.statBox = ctk.CTkFrame(
-                self, width=125, 
-                height=155, 
+                row1, width=100, 
+                height=125, 
                 fg_color=panelColour1, 
                 corner_radius=8,
                 border_width=3,
                 border_color=colour7
                 )
-            self.statBox.pack(side="left", padx=30, pady=20)
+            self.statBox.grid(row=0, column=i, padx=30, pady=15, sticky="nsew")
             self.statBox.pack_propagate(False)
 
             lblStatName = ctk.CTkLabel(
@@ -369,18 +388,110 @@ class App(ctk.CTk):
                 text=key, 
                 anchor="w",
                 fg_color="transparent", 
-                font=("Inter", 16, "bold")
+                font=("Inter", 15, "bold")
                 )
-            lblStatName.pack(pady=5, padx=20)
+            lblStatName.pack(pady=5, anchor="center")
 
             lblStatValue = ctk.CTkLabel(
                 self.statBox, 
                 text=value, 
                 anchor="w",
                 fg_color="transparent", 
-                font=("Inter", 75, "bold")
+                font=("Inter", 60, "bold")
                 )
-            lblStatValue.pack(pady=5, padx=20)
+            lblStatValue.pack(pady=5, anchor="center")
+
+        row2 = ctk.CTkFrame(self.frmGenerationFrame, fg_color="transparent")
+        row2.pack(side="top", anchor="w")
+
+        self.frmClassDetails = ctk.CTkScrollableFrame(
+            row2, width=500, 
+            height=300, 
+            fg_color=panelColour1, 
+            corner_radius=8,
+            border_width=3,
+            border_color=colour7
+            )
+        self.frmClassDetails.pack(side="left", padx=30)
+        self.frmClassDetails.pack_propagate(False)
+
+        self.frmBackground = ctk.CTkScrollableFrame(
+            row2, width=500, 
+            height=300, 
+            fg_color=panelColour1, 
+            corner_radius=8,
+            border_width=3,
+            border_color=colour7
+            )
+        self.frmBackground.pack(side="left", padx=30)
+        self.frmBackground.pack_propagate(False)
+
+        row3 = ctk.CTkFrame(self.frmGenerationFrame, fg_color="transparent")
+        row3.pack(side="top", anchor="w")
+
+        self.frmAppearance = ctk.CTkScrollableFrame(
+            row3, width=500, 
+            height=300, 
+            fg_color=panelColour1, 
+            corner_radius=8,
+            border_width=3,
+            border_color=colour7
+            )
+        self.frmAppearance.pack(side="left", padx=30, pady=20)
+        self.frmAppearance.pack_propagate(False)
+
+        self.frmSkills = ctk.CTkScrollableFrame(
+            row3, width=200, 
+            height=300, 
+            fg_color=panelColour1, 
+            corner_radius=8,
+            border_width=3,
+            border_color=colour7
+            )
+        self.frmSkills.pack(side="left", padx=30, pady=20)
+        self.frmSkills.pack_propagate(False)
+
+        self.frmMiniGenerationFrame = ctk.CTkFrame(row3, fg_color="transparent")
+        self.frmMiniGenerationFrame.pack(side="top", padx=0, pady=0, anchor="w")
+
+        miniRow1 = ctk.CTkFrame(self.frmMiniGenerationFrame, fg_color="transparent")
+        miniRow1.pack(side="top", anchor="w")
+
+        self.frmAllignment = ctk.CTkFrame(
+            miniRow1, width=250, 
+            height=140, 
+            fg_color=panelColour1, 
+            corner_radius=8,
+            border_width=3,
+            border_color=colour7
+            )
+        self.frmAllignment.pack(side="left", padx=30, pady=15)
+        self.frmAllignment.pack_propagate(False)
+
+        self.frmAllignment.configure(height=150)
+
+        miniRow2 = ctk.CTkFrame(self.frmMiniGenerationFrame, fg_color="transparent")
+        miniRow2.pack(side="top", anchor="w")
+        
+
+        self.frmNotes = ctk.CTkScrollableFrame(
+            miniRow2, width=250, 
+            height=140, 
+            fg_color=panelColour1, 
+            corner_radius=8,
+            border_width=3,
+            border_color=colour7
+            )
+        self.frmNotes.pack_propagate(False)
+        self.frmNotes.pack(side="left", padx=30, pady=0)
+        
+        
+
+        
+
+
+
+
 
 
         
